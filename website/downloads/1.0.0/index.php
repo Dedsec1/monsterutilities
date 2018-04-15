cb64739612c0cb45ff90e984a98526c4aa8d9c47
Xerus <27jf@web.de>
2018-04-15 09:53:25 +0200

    Update website & ftp

--- website/downloads/1.0.0/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$filename = "MonsterUtilities.jar";
-header('Content-Disposition: attachment; filename="MonsterUtilities.jar"');
-header('Content-Transfer-Encoding: binary');
-header('Content-Type: application/force-download');
-readfile($filename);
-
-?>
--- website/downloads/unstable/index.php
+++ website/downloads/unstable/index.php
@@ -1,7 +1,9 @@
 <?php
 
-$filename = "MonsterUtilities.jar";
-header("Content-disposition: attachment");
+$filename = glob("*.jar")[0];
+header('Content-Disposition: attachment; filename="'.$filename.'"');
+header('Content-Transfer-Encoding: binary');
+header('Content-Type: application/force-download');
 readfile($filename);
 
 ?>
