9571a37eeae4807f72f138d6d4a304c224a65d48
Xerus <27jf@web.de>
2018-04-22 21:30:00 +0200

    Rewrite website

--- website/downloads/index.php
+++ website/downloads/index.php
@@ -5,13 +5,31 @@ function println($arg = "") {
 	echo "<br>";
 }
 
-$latest = file_get_contents("latest/index.html");
-$ver = $_GET['version']
-$version = isset($ver) && $ver != "latest" ? $ver : $latest
-if(isset($_GET['download'])) {
-
+$cache = array();
+function get($name) {
+	if($cache[$name] == null)
+		$cache[$name] = file_get_contents($name);
+	return $cache[$name];
 }
 
-echo '<a href="/downloads?version='.$latest.'&download">'.$latest.'</a>';
+$ver = $_GET['version'];
+$version = isset($ver) ? switch($ver) {
+	case "latest":
+	case "unstable":
+		get($ver)
+		break;
+	default:
+		$ver
+} : get("latest")
+
+if(isset($_GET['download'])) {
+	$filename = glob("*".$version.".jar")[0];
+	header('Content-Disposition: attachment; filename="'.$filename.'"');
+	header('Content-Transfer-Encoding: binary');
+	header('Content-Type: application/force-download');
+	readfile($filename);
+} else {
+	echo $version;
+}
 
 ?>
--- website/downloads/unstable/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$filename = glob("*.jar")[0];
-header('Content-Disposition: attachment; filename="'.$filename.'"');
-header('Content-Transfer-Encoding: binary');
-header('Content-Type: application/force-download');
-readfile($filename);
-
-?>
